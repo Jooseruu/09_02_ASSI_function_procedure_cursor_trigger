0c20614af5187fd27a962f52f1d8a3c7ad4bf363
Alejandro Roca <119356081+Aleeex04@users.noreply.github.com>
2023-04-27 20:46:33 +0200

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO customers (name, phone) VALUES ('John Smith', '644742134');
 INSERT INTO orders (customer_id, order_date, total_price) VALUES (1, '2023-04-27', 50.00);
-INSERT INTO order_items (order_id, item_name, item_price, quantity) VALUES (1, 'T-Shirt', 20.00, 2);
+INSERT INTO order_items (order_id, item_name, item_price, quantity) VALUES (1, 'Kebab', 20.00, 2);

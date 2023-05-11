fea374e022ceb687a5f6452abb3f911ee5aca147
José Rubén Arjona <120745808+Jooseruu@users.noreply.github.com>
2023-05-11 15:59:30 +0200

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -77,13 +77,40 @@ BEGIN
 END;
 $$;
 
--- Cursor to get all orders for a specific customer
-DECLARE all_orders CURSOR FOR 
-SELECT * FROM orders WHERE customer_id = 1;
+CREATE OR REPLACE FUNCTION get_customer_orders(customer_id INTEGER)
+RETURNS SETOF orders AS $$
+DECLARE 
+    order_row orders%ROWTYPE;
+    all_orders CURSOR FOR 
+        SELECT * FROM orders WHERE customer_id = $1;
+BEGIN 
+    OPEN all_orders;
+    LOOP 
+        FETCH all_orders INTO order_row;
+        EXIT WHEN NOT FOUND;
+        RETURN NEXT order_row;
+    END LOOP;
+    CLOSE all_orders;
+END;
+$$ LANGUAGE plpgsql;
 
 -- Cursor to get all items for a specific order
-DECLARE all_items CURSOR FOR 
-SELECT * FROM order_items WHERE order_id = 1;
+CREATE OR REPLACE FUNCTION get_order_items(order_id INTEGER)
+RETURNS SETOF order_items AS $$
+DECLARE 
+    item_row order_items%ROWTYPE;
+    all_items CURSOR FOR 
+        SELECT * FROM order_items WHERE order_id = $1;
+BEGIN 
+    OPEN all_items;
+    LOOP 
+        FETCH all_items INTO item_row;
+        EXIT WHEN NOT FOUND;
+        RETURN NEXT item_row;
+    END LOOP;
+    CLOSE all_items;
+END;
+$$ LANGUAGE plpgsql;
 
 -- Trigger to update the total price of an order when a new item is added
 CREATE OR REPLACE FUNCTION update_total_price()
